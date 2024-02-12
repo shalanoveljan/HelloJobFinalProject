7b824b0258907280bab82edf1ea30c62b78d8b1c
shalanoveljan <eljanash@code.edu.az>
2024-02-13 01:11:55 +0400

    start courses

--- assets/js/course.js
+++ assets/js/course.js
@@ -72,3 +72,46 @@ loginbtn.addEventListener("click",function (e) {
 
 
 
+
+const likeIcons = document.querySelectorAll('#like');
+const unlikeIcons = document.querySelectorAll('#unlike');
+
+likeIcons.forEach(likeIcon => {
+    likeIcon.addEventListener('click', function(e) {
+        e.preventDefault();
+        this.style.display = 'none';
+        unlikeIcons[Array.from(likeIcons).indexOf(this)].style.display = 'inline-block';
+    });
+});
+
+unlikeIcons.forEach(unlikeIcon => {
+    unlikeIcon.addEventListener('click', function(e) {
+        e.preventDefault();
+        this.style.display = 'none';
+        likeIcons[Array.from(unlikeIcons).indexOf(this)].style.display = 'inline-block';
+    });
+});
+
+const filterClearButton = document.getElementById('clear_filters2');
+const filterClearButton2 = document.getElementById('clear_filters');
+
+filterClearButton.addEventListener('click', function(e) {
+  e.preventDefault()
+    // var selectInputs = document.querySelectorAll('.form-select');
+    // selectInputs.forEach(function(selectInput) {
+    //     selectInput.selectedIndex = 0;
+    // });
+    document.getElementById('sort').selectedIndex = 0;
+    var checkboxes = document.querySelectorAll('.resumes__filters__checkbox input[type="checkbox"]');
+    checkboxes.forEach(function(checkbox) {
+        checkbox.checked = false;
+    });
+});
+filterClearButton2.addEventListener('click', function(e) {
+  e.preventDefault()
+    document.getElementById('sort').selectedIndex = 0;
+    var checkboxes = document.querySelectorAll('.resumes__filters__checkbox input[type="checkbox"]');
+    checkboxes.forEach(function(checkbox) {
+        checkbox.checked = false;
+    });
+});
